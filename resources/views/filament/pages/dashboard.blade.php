34373ebc8f06ccd21f8e9b686318e89f8af27fbe
Hikam Huda <hikamhuda057@gmail.com>
2025-05-21 17:08:48 +0700

    fixasi grafik

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,3 +1,7 @@
-<x-filament-panels::page>
-
-</x-filament-panels::page>
+<x-filament::page>
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+        @livewire(\App\Filament\Widgets\SalesChart::class)
+        @livewire(\App\Filament\Widgets\TransactionsCountChart::class)
+        @livewire(\App\Filament\Widgets\TopProductsChart::class)
+    </div>
+</x-filament::page>
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ Route::get('/receipt/{transaction}/pdf', function (Transaction $transaction) {
     $transaction->load('items.product');
 
     $pdf = Pdf::loadView('receipt', compact('transaction'));
-    return $pdf->download('struk-transaksi-' . $transaction->id . '.pdf');
-})->name('receipt.pdf');
+        return $pdf->download('struk-transaksi-' . $transaction->id . '.pdf');
+        })->name('receipt.pdf');
 
 Route::get('/', function () {
     return redirect('/admin/login');
